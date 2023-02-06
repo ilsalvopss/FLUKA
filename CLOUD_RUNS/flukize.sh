103029910f6a20552eea1068bb962149748e1ba0
Salvo Passaro <ilsalvopss@gmail.com>
2023-02-06 22:41:40 +0100

    CLOUD_RUNS: flukize.sh, initial commit

---
+++ CLOUD_RUNS/flukize.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# @ilsalvopss
+# NOTE: fluka download permitted to authorized users only
+
+fatal(){
+  echo "FATAL ERROR: $1"
+  echo "exiting now.."
+
+  exit 1
+}
+
+if [ -z "$1" ]
+  then
+    fatal "no FLUKA deb provided, specify it as first argument"
+fi
+
+# install FLUKA deb
+sudo apt install $1
+
+if [ $? -ne 0 ]
+  then
+    fatal "apt failed to install FLUKA"
+fi
+
+# install flair repository pubkey
+wget -q -O - https://flair.web.cern.ch/flair/download/ubuntu/flair.gpg | sudo tee /etc/apt/trusted.gpg.d/flair.asc
+
+sudo add-apt-repository 'deb [arch=all,amd64] https://cern.ch/flair/download/ubuntu/22.10 /'
+
+sudo apt install flair
+
+if [ $? -ne 0 ]
+  then
+    fatal "flair installation failed"
+fi
+
+#TODO: apply flair patch
