a00049596b6f92848c5eccfba0a8f6a0f82afc8a
Salvo Passaro <ilsalvopss@gmail.com>
2023-02-20 23:48:17 +0100

    CLOUD_RUNS: start.sh, initial commit

---
+++ CLOUD_RUNS/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# @ilsalvopss
+
+if [ -z "$1" ]
+  then
+    echo "usage: $0 input.flair"
+
+    exit 1
+fi
+
+export PATH=$PATH:/usr/local/fluka/bin
+flair --run "<default>" project.flair $1
